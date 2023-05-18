dcdea23953eea5595a0d7a898f9e7f4aac974641
isaacachenbach <isaac.achenbach1@gmail.com>
2023-05-18 10:30:38 -0500

    created database

--- migration.sql
+++ migration.sql
@@ -1,22 +1,27 @@
-USE adlister_db;
+USE jobseeker_db;
 
-DROP TABLE IF EXISTS ads;
-DROP TABLE IF EXISTS users;
 
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     username VARCHAR(240) NOT NULL,
     email VARCHAR(240) NOT NULL,
     password VARCHAR(255) NOT NULL,
+    company_name VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE ads (
+CREATE TABLE IF NOT EXISTS jobs (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     user_id INT UNSIGNED NOT NULL,
     title VARCHAR(240) NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    salary INT NOT NULL,
+    job_type VARCHAR(240) NOT NULL,
+    shift VARCHAR(240) NOT NULL,
     description TEXT NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES users(id)
         ON DELETE CASCADE
 );
+
+
