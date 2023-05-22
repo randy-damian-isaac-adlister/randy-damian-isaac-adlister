f4a7585d6d392881026ad7ae4492744ba80e435a
isaacachenbach <isaac.achenbach1@gmail.com>
2023-05-22 13:28:25 -0500

    added error messages on register

--- src/main/java/controllers/RegisterServlet.java
+++ src/main/java/controllers/RegisterServlet.java
@@ -11,13 +11,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-
-import static java.lang.System.out;
+import java.util.regex.Pattern;
 
 @WebServlet(name = "controllers.RegisterServlet", urlPatterns = "/register")
 public class RegisterServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        request.getSession().removeAttribute("emptyEmail");
+        request.getSession().removeAttribute("emptyUsername");
+        request.getSession().removeAttribute("emptyPassword");
+        request.getSession().removeAttribute("noMatch");
+        request.getSession().removeAttribute("emailError");
+
         request.getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
+
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -29,47 +36,59 @@ public class RegisterServlet extends HttpServlet {
         request.getSession().setAttribute("company", company);
         request.getSession().setAttribute("username", username);
         request.getSession().setAttribute("email", email);
-        User existingUser = DaoFactory.getUsersDao().findByUsername(username);
 
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-
-
-        // condition to check if username already exists
-        if (existingUser != null){
-            out.println("<script type=\"text/javascript\">");
-            out.println("let form = document.querySelector(\".form-register\")");
-            out.println("let el = document.createElement('h4')");
-            out.println("el.innerHTML = Error: username already exists");
-            out.println("form.appendChild(el)");
-            out.println("console.log(\"test\")");
-            out.println("</script>");
-            response.sendRedirect("/register");
-            return;
+        if (email.isEmpty()) {
+            String msg = "Email field must be filled";
+            request.getSession().setAttribute("emptyEmail", msg);
+            request.getSession().setAttribute("error", "has");
         }
 
-        // validate input
-        boolean inputHasErrors = username.isEmpty()
-            || email.isEmpty()
-            || password.isEmpty()
-            || (! password.equals(passwordConfirmation));
-
-        if (inputHasErrors) {
-            response.sendRedirect("/register");
-            return;
+        if (username.isEmpty()) {
+            String msg = "Username field must be filled";
+            request.getSession().setAttribute("emptyUsername", msg);
+            request.getSession().setAttribute("error", "has");
         }
 
-        // create and save a new user
+        if (password.isEmpty()) {
+            String msg = "Password field must be filled";
+            request.getSession().setAttribute("emptyPassword", msg);
+            request.getSession().setAttribute("error", "has");
+        }
+
+        boolean passwordConfirm = password.equals(passwordConfirmation) || (!password.isEmpty() && !passwordConfirmation.isEmpty());
+        if (passwordConfirm) {
+            String msg = "Passwords must match";
+            request.getSession().setAttribute("noMatch", msg);
+            request.getSession().setAttribute("error", "has");
+        }
+
+        if (!emailIsValid(email)) {
+            String msg = "Email must be valid format (ex. ...@gmail.com)";
+            request.getSession().setAttribute("emailError", msg);
+            request.getSession().setAttribute("error", "has");
+        }
+
         User user = new User(username, email, password, company);
-
-        // hash the password
 
         String hash = Password.hash(user.getPassword());
 
         user.setPassword(hash);
 
         DaoFactory.getUsersDao().insert(user);
-        response.sendRedirect("/login");
 
+        if (emailIsValid(email) && !email.isEmpty() && !username.isEmpty() && !password.isEmpty() && passwordConfirm ){
+            response.sendRedirect("/login");
+        }
+
+    }
+    public static boolean emailIsValid (String email) {
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
+                "[a-zA-Z0-9_+&*-]+)*@" +
+                "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                "A-Z]{2,7}$";
+        Pattern pat = Pattern.compile(emailRegex);
+        if (email == null)
+            return false;
+        return pat.matcher(email).matches();
     }
 }
